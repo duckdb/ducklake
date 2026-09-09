6129ca1a02dee5cc6d7b49ce9e5ecabd494f1b94
Alex Kasko <alex@staticlibs.net>
2026-09-09 11:56:24 +0100

    Enable patches for SQLite on CI

--- .github/config/extensions/sqlite_scanner.cmake
+++ .github/config/extensions/sqlite_scanner.cmake
@@ -9,4 +9,5 @@ duckdb_extension_load(sqlite_scanner
         ${STATIC_LINK_SQLITE} LOAD_TESTS
         GIT_URL https://github.com/duckdb/duckdb-sqlite
         GIT_TAG b71ddb382f14634d1eed5706289ff6c4449b8787
+        APPLY_PATCHES
         )
